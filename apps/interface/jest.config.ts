35d0d6fe8193b05eedd6c55504a9437da712cd78
Mystery <chibueze8141@gmail.com>
2026-04-23 17:31:32 +0000

    test: map @/lib/constants to mock in jest moduleNameMapper

--- apps/interface/jest.config.ts
+++ apps/interface/jest.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
   transform: { "^.+\\.tsx?$": ["ts-jest", { tsconfig: { jsx: "react-jsx" } }] },
   moduleNameMapper: {
     "^@/lib/soroban$": "<rootDir>/src/__mocks__/lib/soroban.ts",
+    "^@/lib/constants$": "<rootDir>/src/__mocks__/lib/constants.ts",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
